54d78d01b73136c7f7a101832015d9bcdc9a6e10
Niko Cremer <n.ko@posteo.de>
2024-02-05 10:47:44 +0100

    added SWRCOfing and fetcher value in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
+import { FETCH_CACHE_HEADER } from "next/dist/client/components/app-router-headers";
 import GlobalStyle from "../styles";
+import { SWRConfig } from "swr";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <SWRConfig value={{ fetcher }}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </SWRConfig>
     </>
   );
 }
